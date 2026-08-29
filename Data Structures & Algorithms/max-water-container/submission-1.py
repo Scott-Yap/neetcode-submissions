47c1430744d8fd89160fe8359d73d911c028efc0
Cheng Hei Scott Yap <149369108+Scott-Yap@users.noreply.github.com>
2026-08-29 08:13:21 +0100

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # use two pointer, then shift when area is larger + the side is shorter
+
+        # lptr = 0
+        # rptr = len(heights) - 1
+        # max_vol = min(heights[lptr], heights[rptr]) * (rptr - lptr)
+
+        # while lptr < rptr:
+        #     a = heights[lptr]
+        #     b = heights[lptr + 1]
+        #     c = heights[rptr - 1]
+        #     d = heights[rptr]
+
+        #     if a >= d:
+        #         max_vol = max(max_vol, min(a,c) * (rptr - 1 - lptr))
+        #         rptr -= 1
+            
+        #     elif a < d:
+        #         max_vol = max(max_vol, min(b,d) * (rptr - 1 - lptr))
+        #         lptr += 1
+            
+        # return max_vol
+
+
+
+
+        lptr = 0
+        rptr = len(heights) - 1
+        max_vol = 0
+
+        while lptr < rptr:
+            current = min(heights[lptr], heights[rptr]) * (rptr - lptr)
+            max_vol = max(max_vol, current)
+
+            if heights[lptr] <= heights[rptr]:
+                lptr += 1
+            else:
+                rptr -= 1
+
+            
+        return max_vol
+
