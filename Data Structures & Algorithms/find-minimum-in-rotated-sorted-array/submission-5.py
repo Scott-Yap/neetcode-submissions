8cbdd1c3afe99a63f2d4efd5f9612c08d2e83ac7
Cheng Hei Scott Yap <149369108+Scott-Yap@users.noreply.github.com>
2026-09-09 02:11:22 +0100

    Add: find-minimum-in-rotated-sorted-array - submission-5

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # do binary search for Olog(n)
+        
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] > nums[right]:
+                left = mid + 1
+                
+            else:
+                right = mid
+        
+        return nums[left]
+
+
+
+
+
+        
