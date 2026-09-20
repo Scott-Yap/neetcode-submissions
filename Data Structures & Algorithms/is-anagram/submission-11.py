8277c17e9ceffc5d5adcc3a61ac934f35ffa625c
Cheng Hei Scott Yap <149369108+Scott-Yap@users.noreply.github.com>
2026-09-20 06:56:37 +0100

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counter_s = dict()
+        counter_t = dict()
+
+        for i in s:
+            counter_s[i] = counter_s.get(i, 0) + 1
+
+        for j in t:
+            counter_t[j] = counter_t.get(j, 0) + 1
+
+        return counter_s == counter_t
+        
